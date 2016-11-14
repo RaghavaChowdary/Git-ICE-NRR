e780a43bfd59351aae2de2acb324ebcdbe2fa638
Student <s525910@ch140029m.nwmissouri.edu>
2016-11-14 16:25:48 -0600

    Initial Commit

---
+++ Git-ICE-NRR/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Git-ICE-NRR
+//
+//  Created by Nallabothula,Raghava Rao on 11/14/16.
+//  Copyright © 2016 Nallabothula,Raghava Rao. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
