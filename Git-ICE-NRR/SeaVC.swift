598d390c86d27eae00551f1b64282e49461089ab
Student <s525910@ch140029m.nwmissouri.edu>
2016-11-14 16:45:40 -0600

    added seaVc view controller change the names of the view controllers

---
+++ Git-ICE-NRR/SeaVC.swift
@@ -0,0 +1,21 @@
+//
+//  SeaVC.swift
+//  Git-ICE-NRR
+//
+//  Created by Nallabothula,Raghava Rao on 11/14/16.
+//  Copyright © 2016 Nallabothula,Raghava Rao. All rights reserved.
+//
+
+import UIKit
+
+class SeaVC: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
